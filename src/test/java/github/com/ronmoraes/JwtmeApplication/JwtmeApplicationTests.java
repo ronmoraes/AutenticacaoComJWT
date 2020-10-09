9901d6a5e77287bcceb4bda334b124fe45b1d8c0
ronmoraes <ronmoraes@msn.com>
2020-10-08 22:34:19 -0300

    Autenticação com JWT

---
+++ src/test/java/github/com/ronmoraes/JwtmeApplication/JwtmeApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.ronmoraes.JwtmeApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
